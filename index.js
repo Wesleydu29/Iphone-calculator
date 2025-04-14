b2dfde54f0f58829d262fc0b20f9699b09928483
Wesley Goarant <Wesleydu29860@gmail.com>
2025-04-14 23:46:03 +0200

    fin du css, début du fichier JS

---
+++ index.js
@@ -0,0 +1,11 @@
+let listButtons = document.querySelectorAll('.buttons button');
+let lastReturn = document.querySelector('.screen .last');
+let currentReturn = document.querySelector('.screen .current');
+
+let firstNumber = null;
+let newNumber = null;
+let calculator = '+';
+
+function reloadScreen() {
+    
+}
